bf83ff5026e314a1602f52a34b60bbc28e3422cf
andrea8980 <andrea@casaloda.it>
2026-04-30 20:28:05 +0200

    introduce a list of articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import { useState } from 'react'
 
+
+const articlesList = [
+  'articolo 1',
+  'articolo 2',
+  'articolo 3',
+]
+
 function App() {
+
+
+  const [article, setArticle] = useState(['articolo 1', 'articolo 2', 'articolo 3']);
+
 
   return (
     <>
+<div className="container glass-card">
+<h1 className="listTitle">List of articles</h1>
+<ul>
+  {articlesList.map((inList, i) => (
+    <li>{inList}</li>
+  ))}
+</ul>
 
+</div>
 
     </>
   )
